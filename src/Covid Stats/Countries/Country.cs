3d586fa0a9ec260351dac178ceffe9303fa6e245
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-08-19 14:37:17 +0200

    Added countries

---
+++ src/Covid Stats/Countries/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidStats.Countries
+{
+    class Country
+    {
+        public string Code { get; private set; }
+
+        public Country(string code)
+        {
+            this.Code = code;
+        }
+    }
+}
---
+++ src/Covid Stats/Countries/countrymanager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidStats.Countries
+{
+    static class CountryManager
+    {
+        private static Dictionary<string, Country> ExistingCountries = new Dictionary<string, Country>();
+
+        public static Country GetCountry(string code)
+        {
+            if(!ExistingCountries.ContainsKey(code))
+            {
+                ExistingCountries[code] = new Country(code);
+            }
+            return ExistingCountries[code];
+        }
+    }
+}
