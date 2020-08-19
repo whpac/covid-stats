ab596610951fcac3a575732975fa20e2b2b79a4b
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-08-19 18:35:25 +0200

    One can fetch data for specific date

--- src/Covid Stats/Countries/Country.cs
+++ src/Covid Stats/Countries/Country.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,9 +27,56 @@ namespace CovidStats.Countries
         public void AddDataRow(DateTime day, CovidDataRow data)
         {
             // This is done to strip the time part from the index
+            int day_number = ParseDayIntoNumber(day);
+            this.DataRows[day_number] = data;
+        }
+
+        /// <summary>
+        /// Returns data for a given date. If there is no data, throws an IndexOutOfRangeException
+        /// </summary>
+        /// <param name="date">Requested date</param>
+        /// <returns>Data about the country</returns>
+        public CovidDataRow GetDataForDate(DateTime date)
+        {
+            int day_number = ParseDayIntoNumber(date);
+            if(this.DataRows.ContainsKey(day_number)) return this.DataRows[day_number];
+            throw new IndexOutOfRangeException();
+        }
+
+        /// <summary>
+        /// Returns array of dates for which there's data
+        /// </summary>
+        /// <returns>Array of dates</returns>
+        public DateTime[] GetAvailableDates()
+        {
+            var keys = this.DataRows.Keys;
+            var dates = new List<DateTime>();
+            foreach(var key in keys)
+            {
+                dates.Add(ParseDayNumberIntoDate(key));
+            }
+            return dates.ToArray();
+        }
+
+        /// <summary>
+        /// Strips the time part from date and calculates the number of days since Jan 1, 2020
+        /// </summary>
+        /// <param name="day">Date</param>
+        /// <returns>Number of whole days since Jan 1, 2020</returns>
+        protected int ParseDayIntoNumber(DateTime day)
+        {
             DateTime first_day = new DateTime(2020, 1, 1);
-            int day_of_pandemic = (int)(day - first_day).TotalDays;
-            this.DataRows[day_of_pandemic] = data;
+            return (int)(day - first_day).TotalDays;
+        }
+
+        /// <summary>
+        /// Calculates a date from the day number
+        /// </summary>
+        /// <param name="day_number">Day number</param>
+        /// <returns>Calculated date</returns>
+        protected DateTime ParseDayNumberIntoDate(int day_number)
+        {
+            return new DateTime(2020, 1, 1).AddDays(day_number);
         }
     }
 }
