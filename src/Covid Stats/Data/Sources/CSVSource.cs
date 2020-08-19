1b293b578d5a2418ac961e55f07810be0fb0b289
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-08-19 18:35:57 +0200

    Implemented CSV parsing

--- src/Covid Stats/Data/Sources/CSVSource.cs
+++ src/Covid Stats/Data/Sources/CSVSource.cs
@@ -5,6 +5,7 @@ using Microsoft.SqlServer.Server;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,7 @@ namespace CovidStats.Data.Sources
 
         public CSVSource(string path)
         {
+            if(path.StartsWith("\"") && path.EndsWith("\"")) path = path.Substring(1, path.Length - 2);
             this.FilePath = path;
         }
 
@@ -26,8 +28,9 @@ namespace CovidStats.Data.Sources
             {
                 // Skip header row
                 if(row_number == 0) continue;
+                if(data_rows[row_number] == "") continue;
 
-                string[] row = data_rows[row_number].Split(',');
+                string[] row = SplitByCommas(data_rows[row_number]);
                 DateTime date_reported = DateTime.ParseExact(row[0], "yyyy-MM-dd", null);
                 string country_code = row[1];
 
@@ -41,6 +44,50 @@ namespace CovidStats.Data.Sources
 
                 CountryManager.GetCountry(country_code).AddDataRow(date_reported, data);
             }
+        }
+
+        protected string[] SplitByCommas(string row)
+        {
+            var columns = new List<string>();
+            var current_column = "";
+            bool is_in_quotes = false;
+            bool is_escaped = false;
+
+            foreach(char c in row)
+            {
+                if(c == ',' && !is_in_quotes)
+                {
+                    columns.Add(current_column);
+                    current_column = "";
+                    continue;
+                }
+
+                
+                if(is_escaped)
+                {
+                    is_escaped = false;
+                    current_column += c;
+                    continue;
+                }
+
+                if(c == '\\')
+                {
+                    is_escaped = true;
+                    continue;
+                }
+
+                if(c == '"')
+                {
+                    is_in_quotes = !is_in_quotes;
+                    continue;
+                }
+
+                current_column += c;
+            }
+
+            if(current_column != "") columns.Add(current_column);
+
+            return columns.ToArray();
         }
     }
 }
