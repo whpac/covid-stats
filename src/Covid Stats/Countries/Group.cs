e73e4e2925f9ef1371b7ce4f292ecdc1b212fbf4
Marcin Szwarc <marcinszwarc@hotmail.com>
2020-08-20 14:38:16 +0200

    Added ability to group countries

---
+++ src/Covid Stats/Countries/Group.cs
@@ -0,0 +1,72 @@
+﻿using CovidStats.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidStats.Countries
+{
+    class Group
+    {
+        public readonly string Name;
+        protected GroupDataInterpolation DataInterpolation;
+        protected HashSet<Country> Countries = new HashSet<Country>();
+
+        public Group(string name, GroupDataInterpolation interpolation = GroupDataInterpolation.LAST_KNOWN)
+        {
+            this.Name = name;
+            this.DataInterpolation = interpolation;
+        }
+
+        public void AddCountry(Country country)
+        {
+            this.Countries.Add(country);
+        }
+
+        public void AddCountry(Country[] countries)
+        {
+            foreach(var country in countries) this.Countries.Add(country);
+        }
+
+        public CovidDataRow GetDataForDate(DateTime date)
+        {
+            var joint_data = new CovidDataRow();
+            foreach(var country in this.Countries)
+            {
+                joint_data += country.GetDataForDate(date);
+            }
+            return joint_data;
+        }
+
+        public DateTime[] GetAvailableDates()
+        {
+            if(this.Countries.Count == 0) return new DateTime[] { };
+
+            HashSet<DateTime> dates = new HashSet<DateTime>(this.Countries.First().GetAvailableDates());
+            
+            if(this.DataInterpolation == GroupDataInterpolation.NONE)
+            {
+                foreach(var country in this.Countries)
+                {
+                    dates.IntersectWith(country.GetAvailableDates());
+                }
+            }
+            else
+            {
+                foreach(var country in this.Countries)
+                {
+                    dates.UnionWith(country.GetAvailableDates());
+                }
+            }
+            return dates.ToArray();
+        }
+    }
+
+    enum GroupDataInterpolation
+    {
+        NONE,
+        LAST_KNOWN
+    }
+}
---
+++ src/Covid Stats/Data/Sources/GroupSource.cs
@@ -0,0 +1,49 @@
+﻿using CovidStats.Countries;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidStats.Data.Sources
+{
+    class GroupSource : DataSource
+    {
+        protected string FilePath;
+        public Group[] Groups { get; protected set; }
+
+        public GroupSource(string path)
+        {
+            if(path.StartsWith("\"") && path.EndsWith("\"")) path = path.Substring(1, path.Length - 2);
+            this.FilePath = path;
+        }
+
+        public void Load()
+        {
+            var file_content = System.IO.File.ReadAllText(this.FilePath);
+            var lines = file_content.Split('\n');
+
+            var group_list = new List<Group>();
+            Group current_group = null;
+
+            for(int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i].Trim();
+                if(line == "") continue;
+                if(line.StartsWith("#")) continue;
+
+                if(line.StartsWith(":"))
+                {
+                    current_group = new Group(line.Substring(1));
+                    group_list.Add(current_group);
+                    continue;
+                }
+
+                current_group.AddCountry(CountryManager.GetCountry(line));
+            }
+
+            this.Groups = group_list.ToArray();
+        }
+    }
+}
